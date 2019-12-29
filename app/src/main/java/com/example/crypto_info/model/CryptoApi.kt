cbe844c0042803e61948ddc7f11caa0482cb8c24
Darnell10 <DarnellOtto@ac.c4q.nyc>
2019-12-28 19:40:38 -0500

    slight change

---
+++ app/src/main/java/com/example/crypto_info/model/CryptoApi.kt
@@ -0,0 +1,10 @@
+package com.example.crypto_info.model
+
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface CryptoApi {
+
+    @GET("")
+    fun getCryptos(): Single<List<CryptoCurrencies>>
+}
